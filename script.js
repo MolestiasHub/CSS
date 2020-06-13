4109f6f9fef27d233bc4d2d42a7af98552b114c5
Molly <holymolestianinqiusition@gmail.com>
2020-06-12 22:34:16 -0700

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("pidor");
+const p = document.querySelector(".head h1")
+const change = document.querySelector(".change")
+
+p.addEventListener("click", function(){
+    console.log("pidor");
+    p.classList.add("change");
+});
